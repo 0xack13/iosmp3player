0ac2fed44810e92d97ae0fe98bef63a33300ecca
Saleh Albatati <0xack13@gmail.com>
2016-01-23 13:42:03 +0300

    FileReader Class

---
+++ iosMp3Player/FileReader.swift
@@ -0,0 +1,15 @@
+//
+//  FileReader.swift
+//  iosMp3Player
+//
+//  Created by 0xack13 on 1/23/16.
+//  Copyright © 2016 0xack13. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class FileReader : NSObject {
+    
+}
